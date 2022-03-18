d57eb848f5cd5087e5c99a08abf424d909e7f316
DangKey <dangvanhieu0109@gmail.com>
2022-03-18 19:28:01 +0700

    up

---
+++ buoi3/string.php
@@ -0,0 +1,34 @@
+<?php 
+	$myString = "Hello this is my string!";
+	echo strlen($myString);
+	echo "<br>";
+	echo str_word_count($myString);
+	echo "<br>";
+	echo strpos($myString, "o");
+	echo "<br>";
+	echo strpos($myString, "i");
+	echo "<br>";
+	echo str_replace("is", "are", $myString);
+	echo "<br>";
+	echo strrpos($myString, "i");
+	echo "<br>";
+ ?>
+
+
+
+<?php 
+	$myString1 = "Dang Van Hieu";
+	echo strlen($myString1);
+	echo "<br>";
+	echo str_word_count($myString1);
+	echo "<br>";
+	echo substr_count(substr($myString1, strpos($myString1, "Hieu"), strlen("Hieu")), "n");
+	echo "<br>";
+	echo strlen("Hieu");
+	echo "<br>";
+	echo str_replace("Van", "VAN", $myString1);
+	echo "<br>";
+	echo strtoupper(substr($myString1, strpos($myString1, "Van"), strlen("Van")));
+	echo "<br>";
+	echo str_replace("Hieu", "21PHP01", $myString1);
+ ?>
