43f911ffd10c21a538eba6f261753532c216284d
DangKey <dangvanhieu0109@gmail.com>
2022-04-06 19:39:55 +0700

    up

--- buoi4/homework2.php
+++ buoi4/homework2.php
@@ -1,27 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Student Form</title>
-</head>
-<body>
+<?php 
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$databaseName = 'myschool';
+$connect = new mysqli($server, $username, $password, $databaseName);
 
-	<h1>Student Form</h1>
-	<form action="#" method="POST" enctype='multipart/form-data'>
-		<p>
-			Họ và tên
-			<input type="text" name="title" >
-		</p>
-		<p>
-			Lớp
-			<input type="text" name="description">
-		</p>
-		<p>
-			Ngày sinh
-			<input type="date" name="publish_date">
-		</p>
-		<p>
-			<input type="submit" name="Add news">
-		</p>
-	</form>
-</body>
-</html>
+
+//Check connection
+if ($connect->connect_error) {
+	die("Connection failed: " . $connect->connect_error);
+} else {
+	echo "Connect success!";
+}
+if (isset($_POST['name'])) {
+	echo "Đã submit";
+	$name = $_POST['name'];
+	$room = $_POST['room'];
+	$quantity = $_POST['quantity'];
+	$year = $_POST['year'];
+
+	$sqlQuery = "INSERT INTO class (name, room, quantity, year) VALUES ('$name', '$room', '$quantity', '$year')";
+	echo "<br>";
+	echo $sqlQuery;
+	//Thực thi câu lệnh SQL
+	$connect->query($sqlQuery);
+
+}
+
+ ?>
+<h1>Student Form</h1>
+<form action="#" method="POST">
+	<p>
+		Name
+		<input type="text" name="name" >
+	</p>
+	<p>
+		Room
+		<input type="text" name="room">
+	</p>
+	<p>
+		Quantity
+		<input type="text" name="quantity">
+	</p>
+	<p>
+		Year
+		<input type="text" name="year">
+	</p>
+	<p>
+		<input type="submit" name="register" value="Register">
+	</p>
+</form>
+
